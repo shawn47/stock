9d5ae18a978c735ec53203ed3f674d1e174a2ea6
yongboxiao <yongboxiao@creditease.cn>
2017-10-11 18:31:47 +0800

    init

---
+++ stock.py
@@ -0,0 +1,170 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+from pandas import DataFrame,Series
+import matplotlib.pyplot as plt
+from dtw import dtw
+from sklearn.metrics.pairwise import euclidean_distances
+import time
+from numpy import inf
+from numpy import NaN
+
+
+# pool = Series(get_index_stocks('000016.XSHG'))
+pool = []
+
+POOLLIST = [
+    '000001.csv',
+    '603019.csv'
+]
+
+dist_threshold = 3
+
+def get_target_stock(target):
+    stockInfo = pd.read_csv('sina-data/%s' % target, index_col='date', parse_dates=True)
+    return stockInfo
+
+def get_stock_pool(poolList = POOLLIST):
+    # raw_pool = []
+    for stockFile in poolList:
+        if stockFile != '002304.csv':
+            stockInfo = pd.read_csv('sina-data/%s' % stockFile, index_col='date', parse_dates=True)
+            pool.append(stockInfo)
+
+def get_price(index, startDate, endDate, priceTag):
+    # print(pool[index].ix[1:3][priceTag])
+    # transform Series into np.ndarray(), then it can be transformed into List
+    priceList = np.array(pool[index][startDate: endDate][priceTag]).tolist()
+    return priceList
+
+def get_target_price(targetStockInfo, startDate, endDate, priceTag):
+    priceList = np.array(targetStockInfo[0][startDate: endDate][priceTag]).tolist()
+    return priceList
+
+def main():
+    print(time.strftime('1 %Y-%m-%d %H:%M:%S', time.localtime()))
+    closeprice = {}
+
+    for i in range(len(pool)):
+        closeprice[i] = get_price(i, '2017-10-11 09:35:00', '2017-10-11 11:30:00', 'closing_price')
+        for k in range(len(closeprice[i])):
+            closeprice[i][k] = (closeprice[i][k] - min(closeprice[i])) / (max(closeprice[i]) - min(closeprice[i]))
+
+            if closeprice[i][k] == NaN or closeprice[i][k] == inf:
+                del closeprice[i]
+                continue
+            else:
+                closeprice[i][k] = float("%.9f" % closeprice[i][k])
+            
+        print(time.strftime('2 %Y-%m-%d %H:%M:%S', time.localtime()))
+
+    for i in range(len(pool)):
+        k = i + 1
+
+        if i != len(pool) - 1:
+            for m in range(k, len(pool)):
+                try:
+                    dist, cost, acc, path = dtw(closeprice[i], closeprice[m], dist = euclidean_distances)
+                    # print(acc)
+                    # print('###')
+                except:
+                    continue
+        else:
+            distance_ = acc[-1][-1]
+            print(distance_)
+            print('***')
+                
+            print(time.strftime('3 %Y-%m-%d %H:%M:%S', time.localtime()))    
+
+            if distance_ < dist_threshold:
+                print(distance_)
+                
+                fig_size = plt.rcParams['figure.figsize']
+                fig_size[0] = 12
+                fig_size[1] = 8
+                
+                fig, ax1 = plt.subplots()
+                ax2 = ax1.twinx()
+            
+                p1 = ax1.plot(range(len(closeprice[i])), closeprice[i], 'r')
+                p1 = ax2.plot(range(len(closeprice[i])), closeprice[m], 'k')
+
+                plt.show()
+                break
+            else:
+                continue
+
+def run(target):
+    targetStockInfo = []
+    targetStockInfo.append(get_target_stock(target))
+    targetStockPriceInfo = get_target_price(targetStockInfo, '2017-10-11 09:35:00', '2017-10-11 11:30:00', 'closing_price')
+
+    for k in range(len(targetStockPriceInfo)):
+        targetStockPriceInfo[k] = (targetStockPriceInfo[k] - min(targetStockPriceInfo)) / (max(targetStockPriceInfo) - min(targetStockPriceInfo))
+
+        if targetStockPriceInfo[k] == NaN or targetStockPriceInfo[k] == inf:
+            del targetStockPriceInfo
+            continue
+        else:
+            targetStockPriceInfo[k] = float("%.9f" % targetStockPriceInfo[k])
+
+
+    print(time.strftime('1 %Y-%m-%d %H:%M:%S', time.localtime()))
+    closeprice = {}
+
+    for i in range(len(pool)):
+        closeprice[i] = get_price(i, '2017-10-11 09:35:00', '2017-10-11 11:30:00', 'closing_price')
+        for k in range(len(closeprice[i])):
+            closeprice[i][k] = (closeprice[i][k] - min(closeprice[i])) / (max(closeprice[i]) - min(closeprice[i]))
+
+            if closeprice[i][k] == NaN or closeprice[i][k] == inf:
+                del closeprice[i]
+                continue
+            else:
+                closeprice[i][k] = float("%.9f" % closeprice[i][k])
+            
+        print(time.strftime('2 %Y-%m-%d %H:%M:%S', time.localtime()))
+
+    # watchList = []
+
+    for i in range(len(pool)):
+        if i != len(pool) - 1:
+            try:
+                dist, cost, acc, path = dtw(targetStockPriceInfo, closeprice[i], dist = euclidean_distances)
+            except:
+                continue
+        else:
+            distance_ = acc[-1][-1]
+            print(distance_)
+            print('***')
+                
+            print(time.strftime('3 %Y-%m-%d %H:%M:%S', time.localtime()))    
+
+            if distance_ < dist_threshold:
+                print(distance_)
+                
+                fig_size = plt.rcParams['figure.figsize']
+                fig_size[0] = 12
+                fig_size[1] = 8
+                
+                fig, ax1 = plt.subplots()
+                ax2 = ax1.twinx()
+            
+                p1 = ax1.plot(range(len(closeprice[i])), closeprice[i], 'r')
+                p1 = ax2.plot(range(len(targetStockPriceInfo)), targetStockPriceInfo, 'k')
+
+                plt.show()
+                break
+            else:
+                continue
+
+if __name__ == '__main__':
+    # main()
+    get_stock_pool()
+    # get_price(1, '2017-10-11 10:05:00', '2017-10-11 11:05:00', 'volume')
+    # main()
+    run('002304.csv')
+    # print(len(pool))
+    # print(pool[1].ix[2]['volume'])
---
+++ stock_data_fetch.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import urllib
+import csv
+import os
+import datetime
+import pandas as pd
+import numpy as np
+
+def retrive_stock_data(stockid, folder):
+	print('downloading %s to %s' % (stockid, folder))
+	url_sina = 'http://money.finance.sina.com.cn/quotes_service/api/json_v2.php/CN_MarketData.getKLineData?symbol=%s&scale=%d&ma=no&datalen=%d' % (stockid, 5, 49)
+	fname = os.path.join(folder, '%s.csv' % stockid[2:])
+	print(url_sina)
+
+	raw_data = urllib.urlopen(url_sina).read()
+	if raw_data != 'null':
+		data_std = raw_data.replace('day','"day"').replace('open', '"open"').replace('high','"high"').replace('low', '"low"').replace('close','"close"').replace('volume', '"volume"')
+		data = eval(data_std)
+
+		if not os.path.isdir(folder):
+			os.mkdir(folder)
+
+		with file(fname,"w") as csvfile: 
+			writer = csv.writer(csvfile)
+
+			writer.writerow(["date", "floor_price", "opening_price", "ceiling_price", "volume", "closing_price"])
+			
+			for item in data:
+				writer.writerow([item['day'], item['low'], item['open'], item['high'], item['volume'], item['close']])
+			#写入多行用writerows
+			# writer.writerows([[0,1,3],[1,2,3],[2,3,4]])
+
+		# urllib.urlretrieve(url_sina, fname)
+
+def update_stock_data(stockid, folder):
+	fname = os.path.join(folder, '%s.csv' % stockid[2:])
+	if not os.path.exists(fname):
+		retrive_stock_data(stockid, folder)
+		return
+
+    # data = pd.read_csv(fname, index_col='Date', parse_dates=True)
+
+    # last_date = data.iloc[0:1].index.tolist()[0]
+    # today = pd.Timestamp(datetime.date.today())
+    # if today - last_date < pd.Timedelta(days=2):
+    #     print('Nothing to update. %s last date is %s.' % (stockid, last_date))
+    #     return
+
+    # print('updatting %s to from %s to %s' % (stockid, last_date.date(), today.date()))
+    # query = [
+    #     ('a', last_date.month - 1),
+    #     ('b', last_date.day),
+    #     ('c', last_date.year),
+    #     ('d', today.month - 1),
+    #     ('e', today.day),
+    #     ('f', today.year),
+    #     ('s', stockid),
+    # ]
+    # url = 'http://table.finance.yahoo.com/table.csv?%s' % urllib.urlencode(query)
+    # temp_file = fname + '.tmp'
+    # urllib.urlretrieve(url, temp_file)
+    # update_data = pd.read_csv(temp_file, index_col='Date', parse_dates=True)
+    # data = data.append(update_data)
+    # data.sort_index(ascending=False, inplace=True)
+    # data.to_csv(fname, mode='w')
+    # os.unlink(temp_file)
+
+
+def stock_list(postfixs, files):
+	if len(files) != len(postfixs):
+		print('error: size of files and postfixs not match.')
+		return
+
+	stocks = []
+	for i in range(len(files)):
+		data = pd.read_csv(files[i], header=None, names=['name', 'id'], dtype={'id': np.string0})
+		data['postfix'] = postfixs[i]
+		stocks.append(data)
+
+	data = pd.concat(stocks)
+	print('%d files. %d stocks.' % (len(files), len(data)))
+	return data
+
+def update_stock_data_batch():
+	slist = stock_list(['sh', 'sz'], ['SH.txt', 'SZ.txt'])
+	for i in range(len(slist)):
+		s = slist.iloc[i]
+		print('postfix: %s. id: %s' % (s['postfix'], s['id']))
+		update_stock_data(s['postfix'].strip() + s['id'].strip(), 'sina-data')
+
+
+if __name__ == '__main__':
+	update_stock_data_batch()
